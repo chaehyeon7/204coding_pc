9f57d995016aad25a6885080a1c73cf77dd2380f
chaehyeon7 <81363193+chaehyeon7@users.noreply.github.com>
2021-06-11 09:40:38 +0900

    프로그래머스Lv1 내적 완성 Commit

--- 프로그래머스Lv1/01음양더하기.py
+++ 프로그래머스Lv1/01음양더하기.py
@@ -1,16 +1,16 @@
 def solution(absolutes, signs):
     answer = 0
     for i in range(len(absolutes)):
-        # True에 해당하는 요소(item)는 더한다.
-        if signs[i] is True: # 아래쪽에 s를 비교
+        # True에 해당하는 요소(item)는 더한다
+        if signs[i] is True:
             answer += absolutes[i]
-        # False 에 해당하는 요소(item)는 뺸다
+        # False에 해당하는 요소(item)는 뺀다
         else:
             answer -= absolutes[i]
     return answer
 
 
 if __name__ == "__main__":
-a = [4, 7, 12]
-s = [True, False, True]
-print(solution(a, s))
+    a = [4, 7, 12]
+    s = [True, False, True]
+    print(solution(a, s))
